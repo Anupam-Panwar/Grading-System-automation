0ac8a53052b3fdcd7499f00c75fdf8cf8e55cf73
Anupam Panwar <anupampanwar12@gmail.com>
2021-06-04 21:33:07 +0530

    Added multiple login message

--- Codes/index.php
+++ Codes/index.php
@@ -2,7 +2,7 @@
     session_start();
     if(isset($_SESSION['id']))
     {
-        header('Location: dashboard.php');
+        header('Location: dashboard.php?error=Multiple login is not allowed !! One user is  already logged in');
         exit();
     }
 ?>
